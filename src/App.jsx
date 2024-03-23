b62ef6560d20b2d76749d2e2ab3fc8de7d9fa348
jkhanjan <jhakhanjan62@gmail.com>
2024-03-24 00:01:41 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import "./App.css";
@@ -12,8 +12,37 @@ import TVDetails from "./components/TVDetails";
 import PersonDetails from "./components/PersonDetails";
 import Trailer from "./components/templates/Trailer";
 import Notfound from "./components/Notfound";
+import image from "../public/emoji.png";
 
 const App = () => {
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 868);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth > 868);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  if (!isDesktop) {
+    return (
+      <div
+        className="w-full flex justify-center items-center  h-screen"
+        style={{ backgroundColor: "black" }}
+      >
+        <h1 className="text-white ">
+          <img className="flex items-center" src={image} alt="" />
+          This site is only available on desktop view.<br/> Switch to desktop view
+        </h1>
+      </div>
+    ); // Render nothing on mobile
+  }
+
   return (
     <div className="bg-[ linear-gradient(to bottom, #000000, #434343)] w-screen overflow-hidden">
       <Routes>
